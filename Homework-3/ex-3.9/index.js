7f1f534c6c45e120f6aed42fb34ac57c8b660a46
DominikaDMT <dominikadmt@gmail.com>
2021-04-02 00:22:47 +0200

    Homework - zjazd 4

--- Homework-3/ex-3.9/index.js
+++ Homework-3/ex-3.9/index.js
@@ -1,25 +1,36 @@
 const searchBtn = $('.search');
 
+users = [];
+
 const findUser = () => {
   const userName = $('input').val();
   const url = `https://api.github.com/users/${userName}`;
+  $('input').val('');
 
-  $.ajax(url).done((response) => {
-    if (response.login) {
-      $('.name strong').text(response.name);
-      $('.username strong').text(response.login);
-      $('.since strong').text(response.created_at.substring(0, 10));
-      $('.image').attr('src', response.avatar_url);
-      $('.alert').text('');
-
-      $('ul').append($(`<li>${userName}</li>`));
-    } else {
-      $('.name strong').text('');
-      $('.username strong').text('');
-      $('.since strong').text('');
-      $('.image').attr('src', '');
-      $('.alert').text("That user doesn't exist");
-    }
+  $.ajax({
+    url,
+    statusCode: {
+      404: function () {
+        $('.name strong').text('');
+        $('.username strong').text('');
+        $('.since strong').text('');
+        $('.image').attr('src', '');
+        $('.alert').text("That user doesn't exist");
+      },
+    },
+  }).done((response) => {
+    $('.name strong').text(response.name);
+    $('.username strong').text(response.login);
+    $('.since strong').text(response.created_at.substring(0, 10));
+    $('.image').attr('src', response.avatar_url);
+    $('.alert').text('');
+    if (users.includes(userName)) return;
+    users.push(userName);
+    $('ul').append($(`<li>${userName}</li>`));
+    $('li').click(function (e) {
+      $('input').val(e.target.outerText);
+      findUser();
+    });
   });
 };
 
